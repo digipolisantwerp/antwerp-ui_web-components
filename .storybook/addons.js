00c8afc82155b9e5de1f5ec432a886b3a1c62042
Jasper Van Proeyen <trianglejuice@mac.com>
2020-07-30 12:27:31 +0200

    Basic ACPaaS UI theme

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,10 +1,8 @@
 import '@storybook/addon-notes/register-panel';
 import '@storybook/addon-viewport/register';
 import { addons } from '@storybook/addons';
-import myTheme from './myTheme';
+import acpaasUITheme from './acpaasUITheme';
 
-/*
 addons.setConfig({
-  theme: myTheme,
+  theme: acpaasUITheme,
 });
-*/
