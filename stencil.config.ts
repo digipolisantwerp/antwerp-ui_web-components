e54575e0ea9bbd0ab24ab96e55edbebbcc36151b
Jasper Van Proeyen <trianglejuice@mac.com>
2020-07-30 09:48:57 +0200

    * Namespace * Audit fixes

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-	namespace: 'acpaas-ui-web-components',
+	namespace: 'acpaas-ui',
 	srcDir: 'src',
 	copy: [
 		{ src: 'services' }
