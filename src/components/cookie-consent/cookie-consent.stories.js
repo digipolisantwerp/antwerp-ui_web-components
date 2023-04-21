a57279c4c461de43bb3b0ad6036ab1d9087edec2
Jasper Van Proeyen <trianglejuice@mac.com>
2023-04-21 14:11:47 +0200

    * Upgraded packages * Cleaned up packages

--- src/components/cookie-consent/cookie-consent.stories.js
+++ src/components/cookie-consent/cookie-consent.stories.js
@@ -1,9 +1,7 @@
 import { storiesOf } from '@storybook/html';
-import { withKnobs, select } from '@storybook/addon-knobs';
 import notes from './readme.md';
 
 storiesOf('CookieConsent', module)
-	.addDecorator(withKnobs)
 	.add('Cookie consent', () => `
 		<aui-cookie-consent></aui-cookie-consent>
 	`, { notes })
